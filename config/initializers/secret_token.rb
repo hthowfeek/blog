3ba48346ee99a254f0ed0eaa72b27cde96643c8e
Haya Thowfeek <hthowfee@qatar.cmu.edu>
2012-04-15 22:31:12 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ac83fe4bb17a607a991c9a76cdbfeb2b9c5d58d027edb70742dca6c578c5761211ebbeebd07bc7661093b52e18bc9e100a1d77de97f5ffa3f2f83adf57a74095'
